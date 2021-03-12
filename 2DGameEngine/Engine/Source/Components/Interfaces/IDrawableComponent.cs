809462caa53fdd212cb8ba8f11efa0bd81bc4176
Lajos Rajna <lajos.rajna@gmail.com>
2021-03-12 14:50:43 +0100

    new interfaces

---
+++ 2DGameEngine/Engine/Source/Components/Interfaces/IDrawableComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameEngine2D.Engine.Source.Interfaces
+{
+    public interface IDrawableComponent
+    {
+        public void Draw(SpriteBatch spriteBatch, GameTime gameTime);
+    }
+}
---
+++ 2DGameEngine/Engine/Source/Components/Interfaces/IUpdatableComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameEngine2D.Engine.Source.Interfaces
+{
+    public interface IUpdatableComponent
+    {
+        public void Update(GameTime gameTime);
+    }
+}
