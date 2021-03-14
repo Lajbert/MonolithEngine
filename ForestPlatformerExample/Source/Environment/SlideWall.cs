a9b970e205dfc08ec49dcd1985abff583b03dcca
Lajos Rajna <lajos.rajna@gmail.com>
2021-03-14 11:54:12 +0100

    small fix

--- ForestPlatformerExample/Source/Environment/SlideWall.cs
+++ ForestPlatformerExample/Source/Environment/SlideWall.cs
@@ -11,6 +11,10 @@ namespace ForestPlatformerExample.Source.Environment
     {
         public SlideWall(Vector2 position, int width, int height) : base(LayerManager.Instance.EntityLayer, null, position)
         {
+            if (width == 0 || height == 0)
+            {
+                throw new Exception("Invalid slide wall dimensions!");
+            }
             AddComponent(new BoxCollisionComponent(this, width, height));
             (GetCollisionComponent() as BoxCollisionComponent).DEBUG_DISPLAY_COLLISION = true;
             AddTag("Environment");
