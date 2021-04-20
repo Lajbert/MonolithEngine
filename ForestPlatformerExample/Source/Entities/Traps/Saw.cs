59f9c1a3a9cba92348082481e84b40742084fbb2
Lajos Rajna <lajos.rajna@gmail.com>
2021-04-20 17:46:12 +0200

    some fixes

--- ForestPlatformerExample/Source/Entities/Traps/Saw.cs
+++ ForestPlatformerExample/Source/Entities/Traps/Saw.cs
@@ -20,6 +20,8 @@ namespace ForestPlatformerExample.Source.Entities.Traps
 
         private float Speed = 0.2f;
 
+        private Vector2 offset = new Vector2(8, 0);
+
         public Saw(AbstractScene scene, Vector2 position, bool horizontalMovement = true) : base (scene.LayerManager.EntityLayer, null, position)
         {
 
@@ -32,9 +34,11 @@ namespace ForestPlatformerExample.Source.Entities.Traps
             HorizontalFriction = 0;
             VerticalFriction = 0;
 
-            sprite = new Sprite(this, Assets.GetTexture("Saw"), new Rectangle(0, 0, 38, 38), origin: new Vector2(19, 19));
+            sprite = new Sprite(this, Assets.GetTexture("Saw"), new Rectangle(0, 0, 38, 38), drawOffset: offset, origin: new Vector2(19, 19));
             AddComponent(sprite);
             HasGravity = false;
+
+            //DEBUG_SHOW_PIVOT = true;
 
             if (horizontalMovement)
             {
@@ -45,7 +49,7 @@ namespace ForestPlatformerExample.Source.Entities.Traps
                 VelocityY = Speed;
             }
 
-            AddComponent(new CircleCollisionComponent(this, 19));
+            AddComponent(new CircleCollisionComponent(this, 19, offset));
         }
 
         public override void FixedUpdate()
